f4ed092f7b794495cf2c719aee990363af5b3f8f
sahil <sahil.sharma@ondc.org>
2024-04-11 18:31:08 +0000

    fix: reverted card route

--- src/order/v2/cart/v2/cart.routes.js
+++ src/order/v2/cart/v2/cart.routes.js
@@ -6,19 +6,19 @@ import CartController from './cart.controller.js';
 const rootRouter = new Router();
 
 const cartController = new CartController();
-
+// -- /:cart_key
 rootRouter.post(
-    '/v2/cart/:userId/:cart_key',
+    '/v2/cart/:userId',
     cartController.addItem,
 );
 
 rootRouter.get(
-    '/v2/cart/:userId/:cart_key',
+    '/v2/cart/:userId',
     cartController.getCartItem,cartTranslator
 );
 
 rootRouter.delete(
-    '/v2/cart/:userId/:cart_key',
+    '/v2/cart/:userId',
     cartController.clearCart,
 );
 
