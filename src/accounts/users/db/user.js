02103bc0e3a1413fb4057462f6eb98e0cad37758
Shubham Joshi <shubhamjoshi@WIL-Admins-Mac-mini.local>
2024-04-26 15:52:57 +0530

    fix: added the address in userProfile schema

--- src/accounts/users/db/user.js
+++ src/accounts/users/db/user.js
@@ -1,5 +1,26 @@
 import mongoose from "mongoose";
 
+
+const AddressSchema = new mongoose.Schema(
+    {
+        door: { type: String, default: null },
+        address_name:{ type: String, default: null },
+        name: { type: String, default: null },
+        building: { type: String, default: null },
+        street: { type: String, default: null },
+        locality: { type: String, default: null },
+        ward: { type: String, default: null },
+        city: { type: String, default: null },
+        state: { type: String, default: null },
+        country: { type: String, default: null },
+        areaCode: { type: String, default: null },
+        tag:{ type: String, default: null },
+        lat:{ type: String, default: null },
+        lng:{ type: String, default: null }
+    },
+    { _id: false }
+);
+
 const  UserSchema = new mongoose.Schema(
     {
         userId: { type: String },
@@ -10,7 +31,8 @@ const  UserSchema = new mongoose.Schema(
         phone_otp : {type : String},
         email : {type : String},
         phone_otp_expiry_date : {type : String},
-        email_otp_expiry_date : {type : String}
+        email_otp_expiry_date : {type : String},
+        address: {type:AddressSchema,default:null}
 },
     { _id: true, timestamps: true }
 );
