f5766323962ef14b56bab412340006c34a1e9cc2
Ritu <ritu@WILASSETA0171.local>
2024-05-02 16:15:08 +0530

    feat:added email on otp

--- src/order/v2/confirm/confirmOrder.controller.js
+++ src/order/v2/confirm/confirmOrder.controller.js
@@ -1,154 +1,214 @@
-import ConfirmOrderService from './confirmOrder.service.js';
-import BadRequestParameterError from '../../../lib/errors/bad-request-parameter.error.js';
+import ConfirmOrderService from "./confirmOrder.service.js";
+import BadRequestParameterError from "../../../lib/errors/bad-request-parameter.error.js";
 // import  Notification from "../../v1/db/notification.js"
-import {sendEmail} from "../../../shared/mailer.js"
+import { sendEmail } from "../../../shared/mailer.js";
 import Notification from "../../v2/db/notification.js";
-import moment from 'moment';
-
+import moment from "moment";
 
 const confirmOrderService = new ConfirmOrderService();
 class ConfirmOrderController {
+  /**
+   * confirm order
+   * @param {*} req    HTTP request object
+   * @param {*} res    HTTP response object
+   * @param {*} next   Callback argument to the middleware function
+   * @return {callback}
+   */
+  confirmOrder(req, res, next) {
+    const { body: orderRequest } = req;
 
-    /**
-    * confirm order
-    * @param {*} req    HTTP request object
-    * @param {*} res    HTTP response object
-    * @param {*} next   Callback argument to the middleware function
-    * @return {callback}
-    */
-    confirmOrder(req, res, next) {
-        const { body: orderRequest } = req;
+    confirmOrderService
+      .confirmOrder(orderRequest)
+      .then((response) => {
+        res.json({ ...response });
+      })
+      .catch((err) => {
+        next(err);
+      });
+  }
 
-        confirmOrderService.confirmOrder(orderRequest).then(response => {
-            res.json({ ...response });
-        }).catch((err) => {
-            next(err);
+  /**
+   * confirm multiple orders
+   * @param {*} req    HTTP request object
+   * @param {*} res    HTTP response object
+   * @param {*} next   Callback argument to the middleware function
+   * @return {callback}
+   */
+  confirmMultipleOrder(req, res, next) {
+    const { body: orderRequests } = req;
+
+    if (orderRequests && orderRequests.length) {
+      confirmOrderService
+        .confirmMultipleOrder(orderRequests)
+        .then((response) => {
+          res.json(response);
+        })
+        .catch((err) => {
+          next(err);
         });
-    }
+    } else throw new BadRequestParameterError();
+  }
 
-    /**
-    * confirm multiple orders
-    * @param {*} req    HTTP request object
-    * @param {*} res    HTTP response object
-    * @param {*} next   Callback argument to the middleware function
-    * @return {callback}
-    */
-    confirmMultipleOrder(req, res, next) {
-        const { body: orderRequests } = req;
+  /**
+   * on confirm order
+   * @param {*} req    HTTP request object
+   * @param {*} res    HTTP response object
+   * @param {*} next   Callback argument to the middleware function
+   * @return {callback}
+   */
+  onConfirmOrder(req, res, next) {
+    const { query } = req;
+    const { messageId } = query;
 
-        if (orderRequests && orderRequests.length) {
+    confirmOrderService
+      .onConfirmOrder(messageId)
+      .then((order) => {
+        res.json(order);
+      })
+      .catch((err) => {
+        next(err);
+      });
+  }
 
-            confirmOrderService.confirmMultipleOrder(orderRequests).then(response => {
-                res.json(response);
-            }).catch((err) => {
-                next(err);
-            });
+  /**
+   * on confirm multiple order
+   * @param {*} req    HTTP request object
+   * @param {*} res    HTTP response object
+   * @param {*} next   Callback argument to the middleware function
+   * @return {callback}
+   */
+  async onConfirmMultipleOrder(req, res, next) {
+    const { query } = req;
+    const { messageIds } = query;
 
-        }
-        else
-            throw new BadRequestParameterError();
-    }
+    if (messageIds && messageIds.length && messageIds.trim().length) {
+      const messageIdArray = messageIds.split(",");
 
-    /**
-    * on confirm order
-    * @param {*} req    HTTP request object
-    * @param {*} res    HTTP response object
-    * @param {*} next   Callback argument to the middleware function
-    * @return {callback}
-    */
-    onConfirmOrder(req, res, next) {
-        const { query } = req;
-        const { messageId } = query;
+      confirmOrderService
+        .onConfirmMultipleOrder(messageIdArray)
+        .then(async (orders) => {
+          // console.log("orders>>>>>>>>>>",JSON.stringify(orders))
+          const userEmails = req.user.decodedToken.email;
+          const userName = req.user.decodedToken.name;
+          const orderIds = orders[0].message.order.id;
+          const emailWithoutNumber=orders[0].message.order.fulfillments[0].end.contact.email
+          const nameWithoutNumber=orders[0].message.order.fulfillments[0].end.location.address.name
 
-        confirmOrderService.onConfirmOrder(messageId).then(order => {
-            res.json(order);
-        }).catch((err) => {
-            next(err);
-        });
-    }
+          console.log('emailWithoutNumber', emailWithoutNumber)
+          console.log('nameWithoutNumber', nameWithoutNumber)
 
-    /**
-    * on confirm multiple order
-    * @param {*} req    HTTP request object
-    * @param {*} res    HTTP response object
-    * @param {*} next   Callback argument to the middleware function
-    * @return {callback}
-    */
-    async onConfirmMultipleOrder(req, res, next) {
-        const { query } = req;
-        const { messageIds } = query;
+          console.log("orderIds>>>>>",orderIds)
+          const itemName = orders[0].message.order.quote.breakup[0].title;
+          const itemQuantity = orders[0].message.order.items[0].quantity.count;
+          const itemPrice = orders[0].message.order.quote.price.value;
+          const estimatedDelivery =
+         orders[0].message.order.fulfillments[0]["@ondc/org/TAT"];
 
-        if (messageIds && messageIds.length && messageIds.trim().length) {
-            const messageIdArray = messageIds.split(",");
+          // Parse the duration using moment.js
+          const duration = moment.duration(estimatedDelivery);
 
-            confirmOrderService.onConfirmMultipleOrder(messageIdArray).then(async orders => {
-                // console.log("orders>>>>>>>>>>",JSON.stringify(orders))
-                const userEmails = req.user.decodedToken?.email
-                const userName = req.user.decodedToken?.name
-                const orderIds = orders[0].message.order.id
-                const itemName = orders[0].message.order.quote.breakup[0].title
-                const itemQuantity = orders[0].message.order.items[0].quantity.count
-                const itemPrice = orders[0].message.order.quote.price.value
-                const estimatedDelivery = orders[0].message.order.fulfillments[0]['@ondc/org/TAT']
+          // Get the days and minutes from the duration
+          let days = duration.days();
 
-                // Parse the duration using moment.js
-                const duration = moment.duration(estimatedDelivery);
+          // If duration is less than 1 day, set days to 1
+          if (days === 0 && duration.asMinutes() < 1440) {
+            console.log("days>>>>>", days);
+            days = "1";
+          } else {
+            days = `${days}`;
+          }
 
-                // Get the days and minutes from the duration
-                let days = duration.days();
-
-                // If duration is less than 1 day, set days to 1
-                if (days === 0 && duration.asMinutes() < 1440) {
-                    console.log("days>>>>>", days)
-                    days = '1'
-                }
-                else {
-                    days = `${days}`
-                }
-
-                console.log('notifications has been created', itemName, itemQuantity, itemPrice, estimatedDelivery)
-                Notification.create({
-                    event_type: 'order_creation',
-                    details: `Order has been Accepted with id: ${orderIds}`,
-                    name: userName || ""
-                }).then(notification => {
-                    console.log('Notification created:', notification);
-                }).catch(error => {
-                    console.error('Error creating notification:', error);
+          console.log(
+            "notifications has been created",
+            itemName,
+            itemQuantity,
+            itemPrice,
+            estimatedDelivery
+          );
+          if(!userEmails && !userName){
+            console.log('userEmails>>>>', userEmails)
+            Notification.create({
+                event_type: "order_creation",
+                details: `Order has been Accepted with id: ${orderIds}`,
+                name: userName,
+              })
+                .then((notification) => {
+                  console.log("Notification created:", notification);
+                })
+                .catch((error) => {
+                  console.error("Error creating notification:", error);
                 });
+           
+              await sendEmail({
+                userEmails,
+                orderIds,
+                HTMLtemplate: "/template/acceptedOrder.ejs",
+                userName: userName || "",
+                subject: "Order Acceptance: Your Order has been Accepted",
+                itemName: itemName,
+                itemQuantity: itemQuantity,
+                itemPrice: itemPrice,
+                estimatedDelivery: days,
+              });
+              res.json(orders);
 
-                if (userEmails && orderIds) {
-                    await sendEmail({
-                        userEmails, orderIds, HTMLtemplate: '/template/acceptedOrder.ejs',
-                        userName: userName || '',
-                        subject: 'Order Acceptance: Your Order has been Accepted',
-                        itemName: itemName,
-                        itemQuantity: itemQuantity,
-                        itemPrice: itemPrice,
-                        estimatedDelivery: days
-                    });
-                }
+          }
+          else if(!emailWithoutNumber && !nameWithoutNumber){
+            console.log('emailWithoutNumber>>>>>>>>>>>>>>>>', emailWithoutNumber)
 
-                res.json(orders);
-            }).catch((err) => {
-                next(err);
-            });
+            Notification.create({
+                event_type: "order_creation",
+                details: `Order has been Accepted with id: ${orderIds}`,
+                name: userName,
+              })
+                .then((notification) => {
+                  console.log("Notification created:", notification);
+                })
+                .catch((error) => {
+                  console.error("Error creating notification:", error);
+                });
+           
+              await sendEmail({
+                userEmails,
+                orderIds,
+                HTMLtemplate: "/template/acceptedOrder.ejs",
+                userName: userName || "",
+                subject: "Order Acceptance: Your Order has been Accepted",
+                itemName: itemName,
+                itemQuantity: itemQuantity,
+                itemPrice: itemPrice,
+                estimatedDelivery: days,
+              });
+              res.json(orders);
 
-        }
-        else
-            throw new BadRequestParameterError();
-    }
+          }
+          else{
+            console.log('ritu>>>>>>>>>>>>>>>>')
 
-    orderDetails(req, res, next) {
-        const { params } = req;
-        const { orderId } = params;
-
-        confirmOrderService.getOrderDetails(orderId).then(orders => {
             res.json(orders);
-        }).catch((err) => {
-            next(err);
+
+          }
+        
+        })
+        .catch((err) => {
+          next(err);
         });
-    }
+    } else throw new BadRequestParameterError();
+  }
+
+  orderDetails(req, res, next) {
+    const { params } = req;
+    const { orderId } = params;
+
+    confirmOrderService
+      .getOrderDetails(orderId)
+      .then((orders) => {
+        // res.json(orders);
+      })
+      .catch((err) => {
+        next(err);
+      });
+  }
 }
 
 export default ConfirmOrderController;
