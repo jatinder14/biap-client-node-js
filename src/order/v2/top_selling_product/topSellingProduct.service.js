853e95bd5f12a6693c187d65dd283a9cb3b91a31
Shubham Joshi <shubhamjoshi@WIL-Admins-Mac-mini.local>
2024-05-20 16:46:10 +0530

    fix: top selling items

--- src/order/v2/top_selling_product/topSellingProduct.service.js
+++ src/order/v2/top_selling_product/topSellingProduct.service.js
@@ -1,7 +1,8 @@
 import OrderMongooseModel from '../../v1/db/order.js';
 
 
-import {protocolSearchItems} from "../../../utils/protocolApis/index.js"
+import { protocolSearchItems } from "../../../utils/protocolApis/index.js"
+import logger from '../../../utils/logger.js';
 class TopSellingService {
     async getTopOrderList() {
         try {
@@ -10,77 +11,48 @@ class TopSellingService {
             const allOrders = await OrderMongooseModel.find({ is_order_confirmed: true });
             // Flatten items from all orders into a single array
             const allItems = allOrders.flatMap(order => order.items);
-            // Filter items to include only those in the specified JSON format
-            const filteredItems = allItems.filter(item => {
-                const product = item.product;
-                return (
-                    product &&
-                    product.id &&
-                    product.subtotal &&
-                    product.time &&
-                    product.time.label &&
-                    product.time.timestamp &&
-                    product.descriptor &&
-                    product.descriptor.name &&
-                    product.quantity &&
-                    product.quantity.unitized &&
-                    product.quantity.unitized.measure &&
-                    product.quantity.unitized.measure.unit &&
-                    product.quantity.unitized.measure.value &&
-                    product.quantity.available &&
-                    product.quantity.available.count &&
-                    product.quantity.maximum &&
-                    product.quantity.maximum.count &&
-                    product.price &&
-                    product.price.currency &&
-                    product.price.value &&
-                    product.price.maximum_value &&
-                    product.tags &&
-                    product.tags.length > 0 &&
-                    product.fulfillments &&
-                    product.fulfillments.length > 0
-                );
-            });
-            
+            // Filter items whose count is greater then 1
+            const filteredItems = allItems.filter(item => item.quantity.count > 1)
+            logger.info('filteredItems_top_selling----------',JSON.stringify(filteredItems))
             // Count occurrences of each product and store their details
             const productCounts = {};
-            filteredItems.forEach(item => {
+         filteredItems.forEach(item => {
                 const productId = item.product.id;
                 if (!productCounts[productId]) {
                     productCounts[productId] = { product: item.product, quantityCount: 0 };
                 }
                 productCounts[productId].quantityCount += parseInt(item.quantity.count);
             });
-            
+
             // Convert object into array
             const productsArray = Object.values(productCounts);
-            
+
             // Sort products by quantity count in descending order
             const sortedProducts = productsArray.sort((a, b) => b.quantityCount - a.quantityCount);
             const productsIdsrray= sortedProducts.map(({product} ) => ( product.id));
 
-            
+
             const response = await protocolSearchItems({});
             const data=await response.data
-            // const itemDetails=data.map((item)=>{ 
-            //     item.item_details.id})
-                const matchedItemDetails = [];
-                productsIdsrray.forEach(productId => {
+            logger.info('topSellingData--------',JSON.stringify(data))
+            const matchedItemDetails = [];
+            productsIdsrray.forEach(productId => {
 
-                    const matchingItem = data?.find(item => productId===item.item_details.id );
-                    if (matchingItem) {
-                        matchedItemDetails.push(matchingItem);
-                    }
-                });
+                const matchingItem = data?.find(item => productId===item.item_details.id );
+                if (matchingItem) {
+                    matchedItemDetails.push(matchingItem);
+                }
+            });
 
-                return matchedItemDetails;            // Find the details of top selling products from searchRequest
-            
+            logger.info('matchedItemDetails', JSON.stringify(matchedItemDetails))
+            return matchedItemDetails;            // Find the details of top selling products from searchRequest
+
         } catch (error) {
             throw error;
         }
     }
-    
-    
+
+
 }
 
 export default TopSellingService;
