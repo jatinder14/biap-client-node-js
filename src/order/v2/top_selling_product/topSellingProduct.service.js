4f45945ac0f92df89b2e7c59901ed34e2ab5bdd0
Shubham Joshi <shubhamjoshi@WIL-Admins-Mac-mini.local>
2024-05-21 11:54:15 +0530

    fix: topselling product

--- src/order/v2/top_selling_product/topSellingProduct.service.js
+++ src/order/v2/top_selling_product/topSellingProduct.service.js
@@ -1,58 +1,44 @@
 import OrderMongooseModel from '../../v1/db/order.js';
 
 
-import { protocolSearchItems } from "../../../utils/protocolApis/index.js"
-import logger from '../../../utils/logger.js';
+import {protocolSearchItems} from "../../../utils/protocolApis/index.js"
+import NoRecordFoundError from '../../../lib/errors/no-record-found.error.js';
 class TopSellingService {
     async getTopOrderList() {
         try {
-
-            // Fetch all orders
-            const allOrders = await OrderMongooseModel.find({ is_order_confirmed: true });
-            // Flatten items from all orders into a single array
-            const allItems = allOrders.flatMap(order => order.items);
-            // Filter items whose count is greater then 1
-            const filteredItems = allItems.filter(item => item.quantity.count > 1)
-            logger.info('filteredItems_top_selling----------',JSON.stringify(filteredItems))
-            // Count occurrences of each product and store their details
-            const productCounts = {};
-         filteredItems.forEach(item => {
-                const productId = item.product.id;
-                if (!productCounts[productId]) {
-                    productCounts[productId] = { product: item.product, quantityCount: 0 };
+          const pipeline = [
+                {
+                  $match: { is_order_confirmed: true } 
+                },
+                {
+                  $unwind: "$items" 
+                },
+                {
+                  $match: { "items.quantity.count": { $gt: 1 } } 
+                },
+                {
+                  $group: { 
+                    _id: null,
+                    itemIds: { $push: "$items.id" }
+                  }
+                },
+                {
+                  $project: { 
+                    _id: 0,
+                    itemIds: 1
+                  }
                 }
-                productCounts[productId].quantityCount += parseInt(item.quantity.count);
-            });
-
-            // Convert object into array
-            const productsArray = Object.values(productCounts);
-
-            // Sort products by quantity count in descending order
-            const sortedProducts = productsArray.sort((a, b) => b.quantityCount - a.quantityCount);
-            const productsIdsrray= sortedProducts.map(({product} ) => ( product.id));
-
-
-            const response = await protocolSearchItems({});
-            const data=await response.data
-            logger.info('topSellingData--------',JSON.stringify(data))
-            const matchedItemDetails = [];
-            productsIdsrray.forEach(productId => {
-
-                const matchingItem = data?.find(item => productId===item.item_details.id );
-                if (matchingItem) {
-                    matchedItemDetails.push(matchingItem);
-                }
-            });
-
-            logger.info('matchedItemDetails', JSON.stringify(matchedItemDetails))
-            return matchedItemDetails;            // Find the details of top selling products from searchRequest
-
+              ];
+          const allOrders = await OrderMongooseModel.aggregate(pipeline);
+              const itemIds = allOrders[0].itemIds;
+            const response = await protocolSearchItems({ itemIds: itemIds.join(',')});
+                            return response.data
         } catch (error) {
             throw error;
         }
     }
-
-
+    
+    
 }
 
 export default TopSellingService;
