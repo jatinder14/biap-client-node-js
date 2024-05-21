384ce06ba27289b8ef3b8996c6fe3716982de416
Shubham Joshi <shubhamjoshi@WIL-Admins-Mac-mini.local>
2024-05-21 19:01:30 +0530

    fix: max count shorting

--- src/order/v2/top_selling_product/topSellingProduct.service.js
+++ src/order/v2/top_selling_product/topSellingProduct.service.js
@@ -7,28 +7,40 @@ class TopSellingService {
     async getTopOrderList() {
         try {
           const pipeline = [
-                {
-                  $match: { is_order_confirmed: true } 
-                },
-                {
-                  $unwind: "$items" 
-                },
-                {
-                  $match: { "items.quantity.count": { $gt: 1 } } 
-                },
-                {
-                    $group: {
-                      _id: null,
-                      itemIds: { $addToSet: "$items.id" }  
-                    }
-                  },
-                  {
-                    $project: {
-                      _id: 0,
-                      itemIds: { $slice: ["$itemIds", 10] } 
-                    }
-                  }
-              ];
+            {
+                $match: { is_order_confirmed: true }
+            },
+            {
+                $unwind: "$items"
+            },
+            {
+                $match: { "items.quantity.count": { $gt: 1 } }
+            },
+            {
+                $group: {
+                    _id: "$items.id",
+                    count: { $max: "$items.quantity.count" }
+                }
+            },
+            {
+                $sort: { count: -1 }
+            },
+            {
+                $limit: 10
+            },
+            {
+                $group: {
+                    _id: null,
+                    itemIds: { $push: "$_id" }
+                }
+            },
+            {
+                $project: {
+                    _id: 0,
+                    itemIds: 1
+                }
+            }
+        ];
           const allOrders = await OrderMongooseModel.aggregate(pipeline);
               const itemIds = allOrders[0].itemIds;
             const response = await protocolSearchItems({ itemIds: itemIds.join(',')});
