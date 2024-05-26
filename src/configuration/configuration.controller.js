983ba9cead14023f6466466040a9ba6192805a7d
vishal chaubey <vishalk.chaubey@thewitslab.com>
2024-05-27 00:19:47 +0530

    feat: footer link apis for content details

--- src/configuration/configuration.controller.js
+++ src/configuration/configuration.controller.js
@@ -3,7 +3,77 @@ import Configuration from "./db/configuration.js"
 export const add = async (req, res, next) => {
     try {
         let payload = req?.body;
+        if (accountNumber) {
+            if (!bankName || !ifscCode || !bankAddress || !accountHolderName) {
+                return res.status(400).json({
+                    success: false,
+                    message: `Bank details should be valid!`,
+                })
+            }
+        }
+        if (finderFee && !finderFeeType) {
+            return res.status(400).json({
+                success: false,
+                message: `Finder fee should be valid!`,
+            })
+        }
+        if (payload?.faq && !Array.isArray(payload?.faq)) {
+            return res.status(400).json({
+                success: false,
+                message: "Frequently ask question should be array!",
+            })
+        } else if (payload?.faq && Array.isArray(payload?.faq) && !payload?.faq?.every(el => el.heading && el.content)) {
+            return res.status(400).json({
+                success: false,
+                message: "Frequently ask question should be valid!",
+            })
+        }
+        if (payload?.aboutus && !Array.isArray(payload?.aboutus)) {
+            return res.status(400).json({
+                success: false,
+                message: "About us should be array!",
+            })
+        } else if (payload?.aboutus && Array.isArray(payload?.aboutus) && !payload?.aboutus?.every(el => el.heading && el.content)) {
+            return res.status(400).json({
+                success: false,
+                message: "About us should be valid!",
+            })
+        }
+        if (payload?.tandc && !Array.isArray(payload?.tandc)) {
+            return res.status(400).json({
+                success: false,
+                message: "Term and condition should be array!",
+            })
+        } else if (payload?.tandc && Array.isArray(payload?.tandc) && !payload?.tandc?.every(el => el.heading && el.content)) {
+            return res.status(400).json({
+                success: false,
+                message: "Term and condition should be valid!",
+            })
+        }
+        if (payload?.shippingpolicy && !Array.isArray(payload?.shippingpolicy)) {
+            return res.status(400).json({
+                success: false,
+                message: "Shipping policy should be array!",
+            })
+        } else if (payload?.shippingpolicy && Array.isArray(payload?.shippingpolicy) && !payload?.shippingpolicy?.every(el => el.heading && el.content)) {
+            return res.status(400).json({
+                success: false,
+                message: "Shipping policy should be valid!",
+            })
+        }
+        if (payload?.cancelpolicy && !Array.isArray(payload?.cancelpolicy)) {
+            return res.status(400).json({
+                success: false,
+                message: "Cancellation policy should be array!",
+            })
+        } else if (payload?.cancelpolicy && Array.isArray(payload?.cancelpolicy) && !payload?.cancelpolicy?.every(el => el.heading && el.content)) {
+            return res.status(400).json({
+                success: false,
+                message: "Cancellation policy should be valid!",
+            })
+        }
         let data = await Configuration.create({
+            "bapId": payload?.bapId || "buyer-app-stage.thewitslab.com",
             "name": payload?.name,
             "logo": payload?.logo,
             "color": payload?.color,
@@ -15,6 +85,11 @@ export const add = async (req, res, next) => {
             "accountHolderName": payload?.accountHolderName,
             "finderFee": payload?.finderFee,
             "finderFeeType": payload?.accountNumber,
+            "faq": payload?.faq,
+            "aboutus": payload?.aboutus,
+            "tandc": payload?.tandc,
+            "shippingpolicy": payload?.shippingpolicy,
+            "cancelpolicy": payload?.cancelpolicy,
         })
         res.status(200).json({
             success: true,
@@ -22,26 +97,49 @@ export const add = async (req, res, next) => {
         })
 
     } catch (error) {
-        res.status(500).json({ "success": false, "message": "Internal Server Error!", error: error?.message });
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error!",
+            error: error?.message
+        });
     }
 }
 
 export const getConfigurations = async (req, res) => {
     try {
-        const { page = 1, limit = 100 } = req.query; // Default page: 1, limit: 10 configurations per page
-        const pageNumber = parseInt(page); // Parse the page number as an integer
-        const limitNumber = parseInt(limit); // Parse the limit as an integer
-        // console.log('configuration fetched:', req.query);
-
-        let configurations = await Configuration.find().skip((pageNumber - 1) * limitNumber) // Skip records based on page number
-            .limit(limitNumber)
-
+        const { bapId = "buyer-app-stage.thewitslab.com", type = "basic" } = req.query;
+        if (!["basic", "bank", "finder", "faq", "aboutus", "tandc", "shippingpolicy", "cancelpolicy"].includes(type)) {
+            return res.status(200).json({
+                success: true,
+                data: {},
+            })
+        }
+        let response;
+        let configurations = await Configuration.findOne({ bapId }).lean().exec()
+        if (type == "basic") {
+            response = {
+                "bapId": configurations?.bapId,
+                "name": configurations?.name,
+                "logo": configurations?.logo,
+                "color": configurations?.color,
+                "image": configurations?.image,
+            }
+        }
+        if (type == "faq") response = configurations?.faq
+        if (type == "aboutus") response = configurations?.aboutus
+        if (type == "tandc") response = configurations?.tandc
+        if (type == "shippingpolicy") response = configurations?.shippingpolicy
+        if (type == "cancelpolicy") response = configurations?.cancelpolicy
         res.status(200).json({
             success: true,
-            data: configurations,
+            data: { bapId: configurations?.bapId, [type]: response },
         })
     } catch (error) {
-        res.status(500).json({ "success": false, "message": "Internal Server Error", error: error?.message });
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error!",
+            error: error?.message
+        });
     }
 
 }
--- src/configuration/db/configuration.js
+++ src/configuration/db/configuration.js
@@ -1,20 +1,28 @@
 import mongoose from "mongoose";
 
-const ConfigurationSchema = new mongoose.Schema(
-    {
-        name: { type: String, default: null },
-        logo: { type: String, default: null },
-        color: { type: String, default: null },
-        image: { type: String, default: null },
-        bankName: { type: String, default: null },
-        ifscCode: { type: String, default: null },
-        accountNumber: { type: String, default: null },
-        bankAddress: { type: String, default: null },
-        accountHolderName: { type: String, default: null },
-        finderFee: { type: String, default: null },
-        finderFeeType: { type: String, default: null },
-    },{ timestamps: true }
-);
+const contentSchema = new mongoose.Schema({
+    heading: { type: String },
+    content: { type: String },
+}, { _id: false })
+
+const ConfigurationSchema = new mongoose.Schema({
+    name: { type: String, default: null },
+    logo: { type: String, default: null },
+    color: { type: String, default: null },
+    image: { type: String, default: null },
+    bankName: { type: String, default: null },
+    ifscCode: { type: String, default: null },
+    accountNumber: { type: String, default: null },
+    bankAddress: { type: String, default: null },
+    accountHolderName: { type: String, default: null },
+    finderFee: { type: String, default: null },
+    finderFeeType: { type: String, default: null },
+    faq: { type: [contentSchema] },
+    aboutus: { type: [contentSchema] },
+    tandc: { type: [contentSchema] },
+    shippingpolicy: { type: [contentSchema] },
+    cancelpolicy: { type: [contentSchema] },
+}, { timestamps: true });
 
 const Configuration = mongoose.model('configuration', ConfigurationSchema, "configuration");
 
