9c48f4e07d108af2b0cb1ca3d7c32f3854c9638e
Ritu <ritu@WILASSETA0171.local>
2024-05-30 19:32:10 +0530

    fix:format document

--- src/utils/emailCron.js
+++ src/utils/emailCron.js
@@ -1,34 +1,39 @@
 import { sendEmail } from "../shared/mailer.js";
-import OrderMongooseModel from '../order/v1/db/order.js';
-import cron from 'cron';
+import OrderMongooseModel from "../order/v1/db/order.js";
+import cron from "cron";
 
-export async function emailCronJob(userEmails, orderId, HTMLtemplate, userName, subject) {
-    const CronJob = cron.CronJob;
-    
-    const task = new CronJob('*/15 * * * * *', async () => {
-        try {
-            await sendEmail({
-                userEmails,
-                orderIds:orderId,
-                HTMLtemplate,
-                userName,
-                subject,
-            });
+export async function emailCronJob(
+  userEmails,
+  orderId,
+  HTMLtemplate,
+  userName,
+  subject
+) {
+  const CronJob = cron.CronJob;
 
-            const updatedOrder = await OrderMongooseModel.findOneAndUpdate(
-                { 'id': orderId },
-                { $set: { 'feedback_send': true } },
-                { new: true }
-            );
+  const task = new CronJob("*/15 * * * * *", async () => {
+    try {
+      await sendEmail({
+        userEmails,
+        orderIds: orderId,
+        HTMLtemplate,
+        userName,
+        subject,
+      });
 
-            if (updatedOrder && updatedOrder.feedback_send === true) {
-                task.stop();
-                console.log("Cron job stopped");
-            }
-        } catch (error) {
-            console.error("Error in cron job:", error);
-        }
-    });
-    task.start();
+      const updatedOrder = await OrderMongooseModel.findOneAndUpdate(
+        { id: orderId },
+        { $set: { feedback_send: true } },
+        { new: true }
+      );
+
+      if (updatedOrder && updatedOrder.feedback_send === true) {
+        task.stop();
+        console.log("Cron job stopped");
+      }
+    } catch (error) {
+      console.error("Error in cron job:", error);
+    }
+  });
+  task.start();
 }
-
