22b9491270a8cbe2deaa48ac7bd9616a7b74042f
Ritu <ritu@WILASSETA0171.local>
2024-07-09 15:10:49 +0530

    fix:multiuser to singleuser

--- src/accounts/users/user.controller.js
+++ src/accounts/users/user.controller.js
@@ -23,17 +23,15 @@ class UserController {
     }
     try {
       const phone_otp_expiry_date = Date.now() + 60000;
-      let userId = uuidv4(), user_id = uuidv4()
+      let userId = uuidv4()
       const existingUser = await User.findOne({ phone });
       if (existingUser && existingUser?.userId) {
         userId = existingUser.userId
       }
-      if (existingUser && existingUser?.user_id) {
-        user_id = existingUser.user_id
-      }
+      
       await User.findOneAndUpdate(
         { phone },
-        { phone, phone_otp: otp, phone_otp_expiry_date, userId, user_id }, // OTP expires in 10 minutes
+        { phone, phone_otp: otp, phone_otp_expiry_date, userId }, // OTP expires in 10 minutes
         { upsert: true, new: true }
       );
       await sendOTPUtil(phone, otp);
@@ -96,7 +94,7 @@ class UserController {
         res.header("Access-Control-Allow-Origin", "*");
         return res.status(400).json({ success: false, message: 'Entered OTP is invalid!' });
       }
-      const token = createJwtToken({ userId: user.userId, user_id: user.user_id, uid: user._id, phone: phone });
+      const token = createJwtToken({ userId: user.userId,  uid: user._id, phone: phone });
 
       user.phone_otp = "";
       await user.save();
@@ -125,7 +123,7 @@ class UserController {
   async userProfile(req, res) {
     try {
       const { body: request, user } = req;
-      let userId = user?.decodedToken?.userId ? user?.decodedToken?.userId : (user?.decodedToken?.user_id || user?.decodedToken?.uid)
+      let userId = user?.decodedToken?.userId ? user?.decodedToken?.userId : (user?.decodedToken?.userId || user?.decodedToken?.uid)
       if (request?.email || request?.phone) {
         const duplicateUser = await User.findOne({ "userId": { $ne: userId }, $or: [{ phone: request?.phone }, { email: request?.email }] });
         lokiLogger.error('duplicateUser', duplicateUser, 'duplicateUser.emaill', duplicateUser?.email, duplicateUser?.phone)
@@ -137,14 +135,14 @@ class UserController {
         }
       }
 
-      const existingUser = await User.findOne({ $or: [{ user_id: user?.decodedToken?.user_id }, { userId: user?.decodedToken?.userId }] });
+      const existingUser = await User.findOne({ $or: [{ userId: user?.decodedToken?.userId }] });
       if (existingUser) {
         if (request.userName) existingUser.userName = request.userName;
         if (request.phone) existingUser.phone = request.phone;
         if (request.email) existingUser.email = request.email;
         if (request.userImage || request.picture) existingUser.userImage = request.userImage ? request.userImage : (request.picture || user?.decodedToken?.picture);
         if (request.address || user?.delivery_address) existingUser.address = request.address || user?.delivery_address;
-        if (request.user_id) existingUser.user_id = request.user_id
+        if (request.userid) existingUser.userid = request.userid
         if (request.cart_key) existingUser.cart_key = request.cart_key
         if (request.wishlist_key) existingUser.wishlist_key = request.wishlist_key
         existingUser.userId = userId
@@ -158,7 +156,7 @@ class UserController {
           email: user?.decodedToken?.email || request.email,
           userImage: request.userImage ? request.userImage : (request.picture || user?.decodedToken?.picture),
           delivery_address: request.address || user?.decodedToken?.address,
-          user_id: request.user_id || userId,
+          userid: request.userid || userId,
           cart_key: request.cart_key || "",
           wishlist_key: request.wishlist_key || "",
           userId
@@ -189,13 +187,10 @@ class UserController {
     const { user } = req;
     let phone = user?.decodedToken?.phone || ""
     let email = user?.decodedToken?.email || ""
-    let user_id = user?.decodedToken?.user_id || ""
     let userId = user?.decodedToken?.userId || ""
     try {
       let query = []
-      if (user_id) {
-        query.push({ user_id })
-      }
+      
       if (userId) {
         query.push({ userId })
       }
@@ -209,7 +204,6 @@ class UserController {
           email: userDetails?.email || "",
           phone: userDetails?.phone || "",
           address: userDetails?.address || "",
-          user_id: userDetails?.user_id || "",
           userId: userDetails?.userId || ""
         },
       });
